eb1ce26197cedba5a0cbbf390b89f7dd9d8b13c1
joshuaoreilly <oreilly.joshua@outlook.com>
2017-11-06 19:12:09 -0500

    Update b and k contants
    Lsim still results in linear, not-occilatory output

--- TracteurEtRemorqueSim.m
+++ TracteurEtRemorqueSim.m
@@ -1,24 +1,24 @@
 clc;clear;
 
-k=100;
-b=10;
-mT=800;
-mR=300;
+k=1000;
+b=10000;
+mT=8000;
+mR=12000;
 
 
 A=[0 1 0 0;-k/mT -b/mT k/mT b/mT;0 0 0 1;k/mR b/mR -k/mR -b/mR]
 B=[0;1/mT;0;0];
-C=[0;0;1;0]
+C=[0 0 1 0]
 D=0;
 
 %interval de temps 
 %we need to add this for the project (choose an appropriate interval)
-t=0:500;
+t=0:1000;
 
 %u = heaviside function (unit step). By default starts at 0, we want it 
 %to stop at some point (by default continues into infinite). In order to 
 %make it stop @ two seconds, we set u(t-2s), so that for t = 2, u = 0
-u=100*heaviside(t)-100*heaviside(t-2);
+u=10000*heaviside(t)-10000*heaviside(t-10);
 
 z=lsim(A,B,C,D,u,t);
 
